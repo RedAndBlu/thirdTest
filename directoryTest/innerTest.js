14aa014d56a24cd11c85715bf5f21f38e6d773e4
RedAndBlue <academyckc@gmail.com>
2018-08-04 23:43:50 +0200

    started a new test repository

---
+++ directoryTest/innerTest.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let user = {
+  "print" : "hello world",
+  "keys" : [1,6,9],
+};
+
+console.log(user.print);
---
+++ test03.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function random_1_to_10(){
+  return Math.round(Math.random() * 10 + 1);
+}
